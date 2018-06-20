c67ee75a4682545152cf8efd37df0a9cd3cc8e23
louwenlong <louwenlong025@qq.com>
2018-06-20 22:35:11 +0800

    jiong

--- src/lou/study/letcode/AddTwoNum2.java
+++ src/lou/study/letcode/AddTwoNum2.java
@@ -1,4 +1,60 @@
 package lou.study.letcode;
 
-public class AddTwoNum {
+// result accepted
+public class AddTwoNum2 {
+    //Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    class Solution {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            int total = l1.val + l2.val;
+            ListNode tail;
+            ListNode head = new ListNode(total % 10);
+            tail = head;
+
+            ListNode tmp1 = l1.next;
+            ListNode tmp2 = l2.next;
+            int flag = total / 10;
+
+            while (tmp1 != null && tmp2 != null) {
+                total = tmp1.val + tmp2.val + flag;
+                ListNode node = new ListNode(total % 10);
+                flag = total / 10;
+                tail.next = node;
+                tail = node;
+                tmp1 = tmp1.next;
+                tmp2 = tmp2.next;
+            }
+            if (tmp1 != null) {
+                addRest(tmp1, tail, flag);
+            } else {
+                if (tmp2 != null) {
+                    addRest(tmp2, tail, flag);
+                } else if (flag != 0){
+                    tail.next = new ListNode(flag);
+                }
+            }
+            return head;
+        }
+
+        private void addRest(ListNode node, ListNode tail, int flag) {
+            int newFlag = flag;
+            while (node != null) {
+                int total = node.val + newFlag;
+                ListNode tmp = new ListNode(total % 10);
+                newFlag = total / 10;
+                tail.next = tmp;
+                tail = tmp;
+                node = node.next;
+            }
+            if (newFlag != 0) tail.next = new ListNode(flag);
+        }
+    }
 }
--- src/lou/study/letcode/Median.java
+++ src/lou/study/letcode/Median.java
@@ -1,4 +1,101 @@
 package lou.study.letcode;
 
+// accepted. note: int -> double
 public class Median {
+    class Solution {
+        public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+            if (nums1.length == 0 && nums2.length == 0) return 0;
+
+            int totalNum = nums1.length + nums2.length;
+            if (totalNum % 2 == 0) {
+                return evenMedian(nums1, nums2);
+            }
+            return oddMedian(nums1, nums2);
+
+        }
+
+        private double oddMedian(int[] nums1, int[] nums2) {
+            int totalNum = nums1.length + nums2.length;
+            int mid = totalNum / 2;
+            if (nums1.length == 0) {
+                return nums2[mid];
+            }
+            if (nums2.length == 0) {
+                return nums1[mid];
+            }
+            int i = 0;
+            int j = 0;
+            while (mid > 0) {
+                if (i == nums1.length) {
+                    return nums2[j + mid];
+                }
+                if (j == nums2.length) {
+                    return nums1[i + mid];
+                }
+                if (nums1[i] > nums2[j]) {
+                    ++j;
+                } else {
+                    ++i;
+                }
+                --mid;
+            }
+            if (i == nums1.length) return nums2[j];
+            if (j == nums2.length) return nums1[i];
+            return nums1[i] < nums2[j] ? nums1[i] : nums2[j];
+        }
+
+        private double evenMedian(int[] nums1, int[] nums2) {
+            int totalNum = nums1.length + nums2.length;
+            int mid = totalNum / 2;
+            if (nums1.length == 0) {
+                return (double) (nums2[mid - 1] + nums2[mid]) / 2;
+            }
+            if (nums2.length == 0) {
+                return (double) (nums1[mid - 1] + nums1[mid]) / 2;
+            }
+
+            if (mid == 1) {
+                return (double) (nums1[0] + nums2[0]) / 2;
+            }
+            int i = 0;
+            int j = 0;
+            while (mid > 1) {
+                --mid;
+                if (i == nums1.length) {
+                    return (double) (nums2[j + mid] + nums2[j + mid + 1]) / 2;
+                }
+                if (j == nums2.length) {
+                    return (double) (nums1[i + mid] + nums1[i + mid + 1]) / 2;
+                }
+                if (nums1[i] > nums2[j]) {
+                    ++j;
+                } else {
+                    ++i;
+                }
+            }
+            if (i == nums1.length) {
+                return (double) (nums2[j] + nums2[j + 1]) / 2;
+            }
+            if (j == nums2.length) {
+                return (double) (nums1[i] + nums1[i + 1]) / 2;
+            }
+            int smallOne, bigOne;
+            if (nums1[i] < nums2[j]) {
+                smallOne = nums1[i];
+                ++i;
+            } else {
+                smallOne = nums2[j];
+                ++j;
+            }
+            if (i == nums1.length) {
+                return (double) (smallOne + nums2[j]) / 2;
+            }
+            if (j == nums2.length) {
+                return (double) (smallOne + nums1[i]) / 2;
+            }
+            bigOne = nums1[i] < nums2[j] ? nums1[i] : nums2[j];
+            return (double) (smallOne + bigOne) / 2;
+
+        }
+    }
 }
