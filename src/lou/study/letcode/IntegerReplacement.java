ec470f4e7c2588f9f98affa84626455c33f3ec82
louwenlong <louwenlong025@qq.com>
2018-06-26 22:23:36 +0800

    change divition to bit opration. but also time excced.

--- src/lou/study/letcode/IntegerReplacement.java
+++ src/lou/study/letcode/IntegerReplacement.java
@@ -5,21 +5,32 @@ public class IntegerReplacement {
         public int integerReplacement(int n) {
             int times = 0;
             while (n != 1) {
+                if (n == 3) {
+                    times += 2;
+                    break;
+                }
+                if (n == 2) {
+                    times++;
+                    break;
+                }
                 if (n % 2 == 1) {
                     if ((n & 3) == 3 && n != 3) {
                         n = n + 1;
                     } else {
                         n = n - 1;
                     }
+                    n = n >> 2;
+                    times += 3;
                 } else {
-                    n = n / 2;
+                    n = n >> 1;
+                    times++;
                 }
-                times++;
+
             }
             return times;
         }
     }
    public static void main(String[] args) {
-        System.out.print(new Solution().integerReplacement(3));
+        System.out.print(new Solution().integerReplacement(8));
    }
 }
