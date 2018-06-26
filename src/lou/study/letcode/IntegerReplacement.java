d4216454cb5e2b773d149c5a13fcb3c7ba80cac1
louwenlong <louwenlong025@qq.com>
2018-06-26 23:15:09 +0800

    should deal max int specailly. max int will occor overflow

--- src/lou/study/letcode/IntegerReplacement.java
+++ src/lou/study/letcode/IntegerReplacement.java
@@ -4,6 +4,9 @@ public class IntegerReplacement {
     static class Solution {
         public int integerReplacement(int n) {
             int times = 0;
+            if (n == Integer.MAX_VALUE) {
+                return 32;
+            }
             while (n != 1) {
                 if (n == 3) {
                     times += 2;
@@ -22,8 +25,11 @@ public class IntegerReplacement {
                     n = n >> 2;
                     times += 3;
                 } else {
-                    n = n >> 1;
-                    times++;
+                    while (n % 2 == 0) {
+                        n = n >> 1;
+                        times++;   
+                    }
+
                 }
 
             }
