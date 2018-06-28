336296f0de6ccc71d27f11ffc64c9c3225c03184
louwenlong <louwenlong025@qq.com>
2018-06-28 23:30:36 +0800

    passed

--- src/lou/study/letcode/array/RemoveElement.java
+++ src/lou/study/letcode/array/RemoveElement.java
@@ -1,4 +1,23 @@
 package lou.study.letcode.array;
 
 public class RemoveElement {
+    public int removeElement(int[] nums, int val) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int empty = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                if (empty >= 0) {
+                    nums[empty] = nums[i];
+                    empty++;
+                }
+            } else {
+                if (empty < 0) {
+                    empty = i;
+                }
+            }
+        }
+        return empty < 0 ? nums.length : empty;
+    }
 }
--- test/lou/study/letcode/array/RemoveElementTest.java
+++ test/lou/study/letcode/array/RemoveElementTest.java
@@ -1,5 +1,23 @@
+package lou.study.letcode.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class RemoveElementTest {
 
+    @Test
+    public void removeElement() {
+        RemoveElement removeElement = new RemoveElement();
+        int[] nums = {0,1,2,2,3,0,4,2};
+        int val = 2;
+        Assert.assertEquals(5, removeElement.removeElement(nums, val));
+        int[] a = new int[0];
+        Assert.assertEquals(0, removeElement.removeElement(a, 0));
+        int[] b = {2};
+        Assert.assertEquals(1, removeElement.removeElement(b, 3));
+        int[] c = {3,2,2,3};
+        Assert.assertEquals(2, removeElement.removeElement(c, 3));
+    }
 }
